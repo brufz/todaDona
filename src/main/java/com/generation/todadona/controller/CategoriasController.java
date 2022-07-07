acec45452df38e341621a7decaf19a375ad62364
Eduardo Franz <eduardo.franz77@gmail.com>
2022-07-07 11:46:57 -0300

    add post put delete

--- src/main/java/com/generation/todadona/controller/CategoriasController.java
+++ src/main/java/com/generation/todadona/controller/CategoriasController.java
@@ -3,10 +3,16 @@ package com.generation.todadona.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +42,22 @@ public class CategoriasController {
 	public ResponseEntity<List<CategoriasModel>> getByCategoria(@PathVariable String categoria){
 		return ResponseEntity.ok(repository.findAllByCategoriaContainingIgnoreCase(categoria));
 	}
+	
+		
+	@PostMapping
+	public ResponseEntity<CategoriasModel> post (@RequestBody CategoriasModel categoria){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<CategoriasModel> put (@RequestBody CategoriasModel categoria){
+		return ResponseEntity.ok(repository.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+	
 	
 }
