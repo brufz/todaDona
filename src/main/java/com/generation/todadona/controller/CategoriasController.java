c150571faeca14938a257e3c585b8a653bff7c58
Math-o-Victor <106315792+Math-o-Victor@users.noreply.github.com>
2022-07-07 10:31:00 -0300

    Enviando getById.

--- src/main/java/com/generation/todadona/controller/CategoriasController.java
+++ src/main/java/com/generation/todadona/controller/CategoriasController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,12 @@ public class CategoriasController {
 	@GetMapping
 	public ResponseEntity<List<CategoriasModel>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CategoriasModel> getById(@PathVariable long id){
+		return repository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+					.orElse(ResponseEntity.notFound().build());
 	}
 }
